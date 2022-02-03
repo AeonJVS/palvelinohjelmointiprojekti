123c0603c8072963b3069a8ba0f167a5be415936
Juuso Ihatsu <juuso.ihatsu@gmail.com>
2022-02-03 15:41:08 +0200

    Update 2
    Made a minor update to BookController.java

---
+++ Bookstore/Bookstore/src/main/java/bookstore/Project/web/BookController.java
@@ -0,0 +1,23 @@
+package bookstore.Project.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import bookstore.Project.domain.Book;
+import exercise.Project.domain.Friend;
+
+@Controller
+public class BookController {
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index(Model model) {
+		return "index";
+	}
+}
