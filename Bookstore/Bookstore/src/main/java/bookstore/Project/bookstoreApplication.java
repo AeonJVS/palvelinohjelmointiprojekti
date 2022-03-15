738826a9589ebc3e5795aebe986cbe7215e27a06
Juuso Ihatsu <juuso.ihatsu@gmail.com>
2022-03-15 20:51:46 +0200

    Update C6.2
    Further security functionalities for the Bookstore-application.

--- Bookstore/Bookstore/src/main/java/bookstore/Project/bookstoreApplication.java
+++ Bookstore/Bookstore/src/main/java/bookstore/Project/bookstoreApplication.java
@@ -12,6 +12,8 @@ import bookstore.Project.domain.Book;
 import bookstore.Project.domain.BookRepository;
 import bookstore.Project.domain.Category;
 import bookstore.Project.domain.CategoryRepository;
+import bookstore.Project.domain.User;
+import bookstore.Project.domain.UserRepository;
 import bookstore.Project.BookstoreApplication;
 import bookstore.Project.domain.Book;
 
@@ -25,7 +27,7 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository bookrepository, CategoryRepository categoryrepository) {
+	public CommandLineRunner bookDemo(BookRepository bookrepository, CategoryRepository categoryrepository, UserRepository userrepository) {
 		return (args) -> {
 			log.info("Saving categories...");
 			Category category1 = new Category("Dystopia");
@@ -37,6 +39,15 @@ public class BookstoreApplication {
 			bookrepository.save(new Book("1984", "George Orwell", 1949, "9780451524935", 20, category1));
 			bookrepository.save(new Book("Animal Farm", "George Orwell", 1945, "9780451526342", 5, category1));
 			bookrepository.save(new Book("Do Androids Dream of Electric Sheep?", "Philip K. Dick", 1968, "9781780220383", 40, category2));
+			
+			log.info("Create admin(admin) and user1(mrbrown) and user2(mrblack)");
+			User admin = new User("admin", "$2a$10$i00QBusgixhWN4ZmeDOPHu6AfoYXfXDNEDEc8coL2RaOgeiJFpKlG", "ADMIN");
+			User user1 = new User("mrBrown", "$2a$10$BTM1r5WmxZxuvlLPxtuhkOwyb3xGyTKTFZAa2ztEF2iK.N8Ikm7Tq", "USER");
+			User user2 = new User("mrBlack", "$2a$10$DNBd1NQi3//0DsgwMc1s2uliiuGP74ANc0gdrN.dC1ghjGCulmbxO", "USER");
+			
+			userrepository.save(user1);
+			userrepository.save(user2);
+			userrepository.save(admin);
 			
 			log.info("fetch all books");
 			for (Book book: bookrepository.findAll()) {
